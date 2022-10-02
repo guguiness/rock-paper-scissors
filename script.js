69ff90f8c1ffe7be8ea9063c6caef2d352b64aea
guguiness <gus.nogueira@protonmail.com>
2022-10-02 12:24:36 -0300

    Create playRound function

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function getComputerChoice() {
 const playerSelection = prompt("Select Rock, Paper or Scissors: ").toLowerCase();
 const computerSelection = getComputerChoice(); 
 
+function playRound(playerSelection, computerSelection) {
+    // Computer chooses ROCK
+    if(computerSelection === 1) {
+
+    }
+    // Computer chooses PAPER
+    if(computerSelection === 2) {
+        
+    }
+    // Computer chooses SCISSORS
+    if(computerSelection === 3) {
+
+    }
+}
