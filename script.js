df987047a9928393cbd0930aaefdbf9ef6dd56cd
guguiness <gus.nogueira@protonmail.com>
2022-10-02 12:23:46 -0300

    Add player and computer selection variables and function to generate computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// 
+function getComputerChoice() {    
+    return Math.floor(Math.random() * 3) + 1;
+} 
+
+const playerSelection = prompt("Select Rock, Paper or Scissors: ").toLowerCase();
+const computerSelection = getComputerChoice(); 
+
